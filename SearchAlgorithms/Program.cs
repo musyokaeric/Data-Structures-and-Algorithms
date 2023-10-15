7ac5234473d512fcb8f3d444a347a4862b8ba683
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-10-15 13:31:58 +0300

    Searching Algorithms

---
+++ SearchAlgorithms/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SearchAlgorithms;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("============================================================");
+Console.WriteLine("Understanding Search Algorithms");
+int[] A = { 15, 21, 47, 84, 96 };
+
+var s1 = new Search();
+
+int index = s1.LinearSearch(A, 5, 96);
+Console.WriteLine("Linear Search: Index " + index);
+Console.ReadKey();
+
+index = s1.BinarySearchIterative(A, 5, 96);
+Console.WriteLine("Binary Search Iterative: Index " + index);
+Console.ReadKey();
+
+index = s1.BinarySearchRecursive(A, 96, 0, 4);
+Console.WriteLine("Binary Search Recursive: Index " + index);
+Console.ReadKey();
---
+++ SearchAlgorithms/Search.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithms
+{
+    public class Search
+    {
+        public int LinearSearch(int[] A, int n, int key)
+        {
+            int index = 0;
+            while (index < n)
+            {
+                if (A[index] == key)
+                    return index;
+                index += 1;
+            }
+            return -1;
+        }
+
+        public int BinarySearchIterative(int[] A, int n, int key)
+        {
+            int LEFT = 0;
+            int RIGHT = n - 1;
+
+            while (LEFT <= RIGHT)
+            {
+                int MIDDLE = (LEFT + RIGHT) / 2;
+                if (key == A[MIDDLE])
+                    return MIDDLE;
+                else if (key < A[MIDDLE])
+                    RIGHT = MIDDLE - 1;
+                else if (key > A[MIDDLE])
+                    LEFT = MIDDLE + 1;
+            }
+            return -1;
+        }
+
+        public int BinarySearchRecursive(int[] A, int key, int LEFT, int RIGHT)
+        {
+            if (LEFT > RIGHT) return -1;
+            else
+            {
+                int MIDDLE = (LEFT + RIGHT) / 2;
+                if (key == A[MIDDLE])
+                    return MIDDLE;
+                else if (key < A[MIDDLE])
+                    return BinarySearchRecursive(A, key, LEFT, MIDDLE - 1);
+                else if (key > A[MIDDLE])
+                    return BinarySearchRecursive(A, key, MIDDLE + 1, RIGHT);
+            }
+            return -1;
+        }
+    }
+}
