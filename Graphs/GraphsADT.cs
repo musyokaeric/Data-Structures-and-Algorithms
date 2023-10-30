f8bcedd7b6be984781efed4c11848a34628dc724
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2023-10-30 10:07:37 +0300

    Graphs

---
+++ Graphs/GraphsADT.cs
@@ -0,0 +1,122 @@
+﻿using Queues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class GraphsADT
+    {
+        int vertices;
+        int[,] adjacentMatrix;
+        int[] visited;
+
+        public GraphsADT(int n)
+        {
+            vertices = n;
+            adjacentMatrix = new int[n, n];
+            visited = new int[vertices];
+        }
+
+        public int VertexCount => vertices;
+
+        public int EdgeCount()
+        {
+            int count = 0;
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                    if (adjacentMatrix[i, j] != 0)
+                        count++;
+            }
+            return count;
+        }
+
+        public void InsertEdge(int u, int v, int x)
+        {
+            adjacentMatrix[u, v] = x;
+        }
+
+        public void RemoveEdge(int u, int v)
+        {
+            adjacentMatrix[u, v] = 0;
+        }
+
+        public bool ExistsEdge(int u, int v) => adjacentMatrix[u, v] != 0;
+
+        public void Edges()
+        {
+            Console.WriteLine("Edges:");
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                    if (adjacentMatrix[i, j] != 0)
+                        Console.WriteLine(i + " -- " + j);
+            }
+        }
+
+        public int OutDegree(int v)
+        {
+            int count = 0;
+            for (int i = 0; i < vertices; i++)
+                if (adjacentMatrix[v, i] != 0)
+                    count++;
+            return count;
+        }
+
+        public int InDegree(int v)
+        {
+            int count = 0;
+            for (int i = 0; i < vertices; i++)
+                if (adjacentMatrix[i, v] != 0)
+                    count++;
+            return count;
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                    Console.Write(adjacentMatrix[i, j] + "\t");
+                Console.WriteLine();
+            }
+        }
+
+        public void BreadthFirstSearch(int s) // first-level vertex
+        {
+            int i = s;
+            QueuesLinked q = new QueuesLinked();
+            Console.Write(i + " -> ");
+            visited[i] = 1;
+            q.Enqueue(i);
+            while (!q.IsEmpty)
+            {
+                i = q.Dequeue();
+                for (int j = 0; j < vertices; j++)
+                {
+                    if (adjacentMatrix[i, j] == 1 && visited[j] == 0)
+                    {
+                        Console.Write(j + " -> ");
+                        visited[j] = 1;
+                        q.Enqueue(j);
+                    }
+                }
+            }
+        }
+
+        public void DepthFirstSearch(int s) // first-level vertex
+        {
+            if (visited[s] == 0)
+            {
+                Console.Write(s + " -> ");
+                visited[s] = 1;
+                for (int j = 0; j < vertices; j++)
+                    if (adjacentMatrix[s, j] == 1 && visited[j] == 0)
+                        DepthFirstSearch(j);
+            }
+        }
+    }
+}
---
+++ Graphs/Program.cs
@@ -0,0 +1,95 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Graphs;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("============================================================");
+Console.WriteLine("Graphs Abstract Data Types");
+
+GraphsADT graph1 = new GraphsADT(4);
+graph1.Display();
+Console.WriteLine("Vertices count: " + graph1.VertexCount);
+Console.WriteLine("Edges count: " + graph1.EdgeCount());
+Console.WriteLine();
+
+graph1.InsertEdge(0, 1, 26);
+graph1.InsertEdge(0, 2, 16);
+graph1.InsertEdge(1, 0, 26);
+graph1.InsertEdge(1, 2, 12);
+graph1.InsertEdge(2, 0, 16);
+graph1.InsertEdge(2, 1, 12);
+graph1.InsertEdge(2, 3, 8);
+graph1.InsertEdge(3, 2, 8);
+
+Console.WriteLine();
+Console.WriteLine("Graphs Adjacency Matrix:");
+graph1.Display();
+Console.WriteLine("Vertices count: " + graph1.VertexCount);
+Console.WriteLine("Edges count: " + graph1.EdgeCount());
+graph1.Edges();
+Console.WriteLine();
+
+Console.WriteLine("Edge between 1--3: " + graph1.ExistsEdge(1, 3));
+Console.WriteLine("Edge between 1--2: " + graph1.ExistsEdge(1, 2));
+Console.WriteLine("Degree of Vertex 2: " + graph1.InDegree(2));
+Console.WriteLine();
+
+Console.WriteLine("Graphs Adjacency Matrix:");
+graph1.Display();
+Console.WriteLine();
+
+Console.WriteLine("Graphs Adjacency Matrix:");
+graph1.RemoveEdge(1, 2);
+graph1.Display();
+Console.WriteLine("Edge between 1--2: " + graph1.ExistsEdge(1, 2));
+Console.ReadKey();
+
+Console.WriteLine();
+Console.WriteLine("Breadth First Search");
+Console.WriteLine("============================================================");
+Console.WriteLine();
+
+GraphsADT graph2 = new GraphsADT(7);
+graph2.InsertEdge(0, 1, 1);
+graph2.InsertEdge(0, 5, 1);
+graph2.InsertEdge(0, 6, 1);
+graph2.InsertEdge(1, 0, 1);
+graph2.InsertEdge(1, 2, 1);
+graph2.InsertEdge(1, 5, 1);
+graph2.InsertEdge(1, 6, 1);
+graph2.InsertEdge(2, 3, 1);
+graph2.InsertEdge(2, 4, 1);
+graph2.InsertEdge(2, 6, 1);
+graph2.InsertEdge(3, 4, 1);
+graph2.InsertEdge(4, 2, 1);
+graph2.InsertEdge(4, 5, 1);
+graph2.InsertEdge(5, 2, 1);
+graph2.InsertEdge(5, 3, 1);
+graph2.InsertEdge(6, 3, 1);
+graph2.BreadthFirstSearch(0); //first level vertex
+Console.ReadKey();
+
+Console.WriteLine();
+Console.WriteLine("Depth First Search");
+Console.WriteLine("============================================================");
+Console.WriteLine();
+
+GraphsADT graph3 = new GraphsADT(7);
+graph3.InsertEdge(0, 1, 1);
+graph3.InsertEdge(0, 5, 1);
+graph3.InsertEdge(0, 6, 1);
+graph3.InsertEdge(1, 0, 1);
+graph3.InsertEdge(1, 2, 1);
+graph3.InsertEdge(1, 5, 1);
+graph3.InsertEdge(1, 6, 1);
+graph3.InsertEdge(2, 3, 1);
+graph3.InsertEdge(2, 4, 1);
+graph3.InsertEdge(2, 6, 1);
+graph3.InsertEdge(3, 4, 1);
+graph3.InsertEdge(4, 2, 1);
+graph3.InsertEdge(4, 5, 1);
+graph3.InsertEdge(5, 2, 1);
+graph3.InsertEdge(5, 3, 1);
+graph3.InsertEdge(6, 3, 1);
+graph3.DepthFirstSearch(0); //first level vertex
+Console.ReadKey();
